f05ca2f67f2bd1f887058144efbc4fde9a2486b6
Mootassam <mootassame@gmail.com>
2023-05-26 14:59:53 +0000

    add the more width of canvas in back china

--- src/App.tsx
+++ src/App.tsx
@@ -34,8 +34,11 @@ function App() {
   const [value, setValue] = useState("alipay");
   const [amount, setAmount] = useState(100);
   const [size, setSize] = useState("small");
-  const [{ cursor, canvasRef, color }, { startDrawing, draw, endDrawing, ...api }] = DrawingApp()
-  const toolbarProps = { color, ...api }
+  const [
+    { cursor, canvasRef, color },
+    { startDrawing, draw, endDrawing, ...api },
+  ] = DrawingApp();
+  const toolbarProps = { color, ...api };
   return (
     <>
       <Header />
@@ -51,14 +54,27 @@ function App() {
         />
         <div className="content" ref={divRef}>
           <div className="app__canvas">
-            <canvas ref={canvasRef as any}
-              width={360}
-              height={820}
-              style={{ cursor }}
-              onMouseDown={startDrawing}
-              onMouseMove={draw}
-              onMouseUp={endDrawing}
-            />
+            {value === "BankChina" ? (
+              <canvas
+                ref={canvasRef as any}
+                width={384}
+                height={801}
+                style={{ cursor }}
+                onMouseDown={startDrawing}
+                onMouseMove={draw}
+                onMouseUp={endDrawing}
+              />
+            ) : (
+              <canvas
+                ref={canvasRef as any}
+                width={360}
+                height={820}
+                style={{ cursor }}
+                onMouseDown={startDrawing}
+                onMouseMove={draw}
+                onMouseUp={endDrawing}
+              />
+            )}
           </div>
           {value === "800" && size === "small" && <Hkd amount={amount} />}
           {value === "800" && size === "large" && <Bank amount={amount} />}
